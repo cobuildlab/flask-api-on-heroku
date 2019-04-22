8e86ad2d872ddcf5b55c71b7db3a902e57bf43d3
Angel Lacret <alacret@gmail.com>
2019-04-22 13:07:46 -0400

    commit mesage

--- app.py
+++ app.py
@@ -1,55 +1,32 @@
 from flask import Flask, jsonify
-from pymongo import MongoClient
 
-app = Flask(__name__)
-client = MongoClient("mongodb://127.0.0.1:27017")  # host uri
-db = client.mymongodb  # Select the database
-tasks_collection = db.task  # Select the collection name
-initial_tasks = [task for task in tasks_collection.find()]
-
-if (len(initial_tasks)) == 0:
-    tasks_collection.insert({
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    })
-    tasks_collection.insert({
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    })
+tasks = []
 
 
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
-    all_tasks = tasks_collection.find()
     task_list = []
-    for task in all_tasks:
+    for task in tasks:
         task_list.append({'title': task['title'], 'description': task['description'], 'id': task['id']})
-
     return jsonify({'tasks': task_list})
 
 
 @app.route('/api/create-task', methods=['GET'])
 def create_task():
-    tasks = tasks_collection.find()
-    new_task = {"id": tasks.count(), "title": "Learn Mongo", "description": "Start with Flask + Mongo", "done": False}
-    tasks_collection.insert(new_task)
-    all_tasks = tasks_collection.find()
-    task_list = []
-    for task in all_tasks:
+    new_task = {"id": len(tasks), "title": "Learn Heroku", "description": "Start with Flask ", "done": False}
+    tasks.append(new_task)
+    for task in tasks:
         task_list.append({'title': task['title'], 'description': task['description'], 'id': task['id']})
     return jsonify({'tasks': task_list})
 
 
 @app.route('/api/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    tasks = tasks_collection.find({'id': task_id})
-    if tasks.count() == 0:
-        return jsonify({'task': None})
-    return jsonify({'task': tasks[0]})
+    try:
+        task = tasks[task_id]
+    except:
+        return jsonify({"text": "Error, task not found"})
+    return jsonify({'title': task['title'], 'description': task['description'], 'id': task['id']})
 
 
 @app.route('/', methods=['GET'])
